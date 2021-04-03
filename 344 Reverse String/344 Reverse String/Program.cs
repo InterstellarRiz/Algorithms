268ef5433112ce5f8c1276d120b4d1250c9ac2b8
John <76816408+JohnRizor@users.noreply.github.com>
2021-04-02 23:50:21 -0400

    Add files via upload

---
+++ 344 Reverse String/344 Reverse String/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _344_Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a sample array
+            char[] s = { 'h', 'e', 'l', 'l', 'o' };
+            reverseString(s);
+        }
+        public static void reverseString(char[] reverseS)
+        {
+            // easy MVP product
+            //Array.Reverse(reverseS);
+
+            // another method 
+            // initialize 
+            char[] reverseString = new char[reverseS.Length];
+            int counter = 0;
+            //loop -- reverse the loop and add the counter
+            for (int i = reverseS.Length - 1; i >= 0; i--)
+            {
+                reverseString[counter] = reverseS[i];
+                counter++;
+            }
+            Array.Copy(reverseString, reverseS, reverseS.Length);
+
+            //compare
+
+
+        }
+    }
+}
