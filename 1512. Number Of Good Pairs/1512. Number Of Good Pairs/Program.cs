addaf261bb2eb657e6397c167aa6a0f734e21590
John <76816408+JohnRizor@users.noreply.github.com>
2021-04-03 22:44:28 -0400

    Add files via upload

---
+++ 1512. Number Of Good Pairs/1512. Number Of Good Pairs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1512._Number_Of_Good_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] identicalPairs = { 1, 2, 3, 1, 1, 3 };
+            GoodPairs(identicalPairs);
+        }
+
+        static int GoodPairs(int[] nums)
+        {
+            int goodPairs = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i < j)
+                    {
+                        //if (i < j)
+                        //{
+                        goodPairs++;
+                        //}
+                    }
+
+
+                }
+
+            }
+            return goodPairs;
+        }
+    }
+}
