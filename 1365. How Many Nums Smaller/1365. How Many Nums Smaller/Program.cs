a5830029a1c718450fc1301f5f1498136161ad0a
John <76816408+JohnRizor@users.noreply.github.com>
2021-04-06 00:02:04 -0400

    Add files via upload

---
+++ 1365. How Many Nums Smaller/1365. How Many Nums Smaller/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1365._How_Many_Nums_Smaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 8, 1, 2, 2, 3 };
+            SmallerNumbersThanCurrent(numbers);
+        }
+
+        public static int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            List<int> smallCount = new List<int>();
+            int counter = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                counter = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        counter++;
+                    }
+                }
+                smallCount.Add(counter);
+            }
+            int[] smallArray = smallCount.ToArray();
+            return smallArray;
+        }
+    }
+}
