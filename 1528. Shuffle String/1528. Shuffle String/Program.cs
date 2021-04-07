f4467bec7c201643ae7885d4bfa46619e734782a
John <76816408+JohnRizor@users.noreply.github.com>
2021-04-06 22:45:24 -0400

    Add files via upload

---
+++ 1528. Shuffle String/1528. Shuffle String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1528._Shuffle_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] order = new int[8];
+            ShuffleString("codeleet", order);
+        }
+
+        static string ShuffleString(string s, int[] indices)
+        {
+            // string and the array length are the same length
+            // attach the letters of an array to the represented array index ... 
+            // ex/ arr[0] = a ... arr[1] == b ...
+            char[] shuffleString = new char[s.Length];
+            for (int i = 0; i < indices.Length; i++)
+            {
+                shuffleString[indices[i]] = s[i];
+            }
+            return new string(shuffleString);
+        }
+    }
+}
