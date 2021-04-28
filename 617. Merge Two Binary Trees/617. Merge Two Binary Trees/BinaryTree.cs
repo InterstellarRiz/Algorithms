2326750b977f7e851b0417b2ba395461246a8cc3
John <76816408+JohnRizor@users.noreply.github.com>
2021-04-27 21:11:28 -0400

    Add files via upload

---
+++ 617. Merge Two Binary Trees/617. Merge Two Binary Trees/BinaryTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _617._Merge_Two_Binary_Trees
+{
+    class BinaryTree
+    {
+        private TreeNode _root;
+        public BinaryTree()
+        {
+            _root = null;
+        }
+        public void Insert(int nodeValue)
+        {
+            //if the tree is empty, return a new single node
+            if(_root == null)
+            {
+                _root = new TreeNode(nodeValue);
+                return;
+            }
+            // else start the tree
+            InsertNode(_root, new TreeNode(nodeValue));
+        }
+        public void InsertNode(TreeNode root, TreeNode newNode)
+        {
+            if (root == null)
+                root = newNode;
+
+            if(newNode.val < root.val)
+            {
+                if (root.left == null)
+                    root.left = newNode;
+                else
+                    InsertNode(root.left, newNode);
+            }
+            else
+            {
+                if (root.right == null)
+                    root.right = newNode;
+                else
+                    InsertNode(root.right, newNode);
+            }
+        }
+
+        public void DisplayTree(TreeNode root)
+        {
+            if (root == null) return;
+            DisplayTree(root.left);
+            Console.WriteLine(root.val + " ");
+            DisplayTree(root.right);
+        }
+        public void DisplayTree()
+        {
+            DisplayTree(_root);
+        }
+    }
+}
---
+++ 617. Merge Two Binary Trees/617. Merge Two Binary Trees/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _617._Merge_Two_Binary_Trees
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree1 = new BinaryTree();
+            TreeNode root1 = new TreeNode();
+            tree1.Insert(1);
+            tree1.Insert(3);
+            tree1.Insert(2);
+            tree1.Insert(5);
+
+            BinaryTree tree2 = new BinaryTree();
+            TreeNode root2 = new TreeNode();
+            tree1.Insert(2);
+            tree1.Insert(1);
+            tree1.Insert(3);
+            tree1.Insert(7);
+            tree1.Insert(1);
+            tree1.Insert(3);
+
+            MergeTrees(tree1, tree2);
+
+
+        }
+
+        public static TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            // if both of the head/roots are null then return null ...
+            if (root1 == null && root2 == null)
+            {
+                return null;
+            }
+            TreeNode twoTreesMerged = new TreeNode(0);
+            if(root1 == null && root2 != null)
+            {
+                twoTreesMerged.val = root2.val;
+                twoTreesMerged.left = MergeTrees(root2.left, null);
+                twoTreesMerged.right = MergeTrees(null, root2.right);
+            }
+            else if(root1 != null && root2 == null)
+            {
+                twoTreesMerged.val = root1.val;
+                twoTreesMerged.left = MergeTrees(root1.left, null);
+                twoTreesMerged.left = MergeTrees(null, root1.right);
+            }
+            else
+            {
+                twoTreesMerged.left = MergeTrees(root1.left, root2.left);
+                twoTreesMerged.right = MergeTrees(root1.right, root2.right);
+                twoTreesMerged.val = root1.val + root2.val;
+            }
+            return twoTreesMerged;
+
+        }
+    }
+
+}
