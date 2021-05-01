7fc264b75789707b68fb22743498a37c8e34b789
John <76816408+JohnRizor@users.noreply.github.com>
2021-05-01 10:53:08 -0400

    Add files via upload

---
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _121._Best_Time_to_Buy_and_Sell_Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            int bestDay = MaxProfit(prices);
+
+            Console.WriteLine(bestDay);
+        }
+
+        static int MaxProfit(int[] prices)
+        {
+            //we want to find the days of the week that will give the lowest buy options and highest sell option
+            //go through the array and compare the value to the next value 
+            // what is the difference and store it ...
+            // you want to iterate through the array and compare the next value to the current value ... nest for loop
+
+            //initialize
+            int greatestProfit = 0;
+            int highestPrice = 0;
+            int lowestPrice = int.MaxValue;
+
+            //loop
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if(prices[i] < lowestPrice)
+                {
+                    lowestPrice = prices[i];
+                }
+                else if(prices[i] - lowestPrice > highestPrice)
+                {
+                    highestPrice = prices[i] - lowestPrice;
+                }
+            }
+            return highestPrice;
+
+            // O(N^2) time ... too long on LeetCode
+            /*for (int i = 0; i < prices.Length; i++) // {7,1,5,3,6,4}
+            {
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    // we want to compare and store the different
+                    if(prices[j] - prices[i] > greatestProfit) // you want to subtract j from i because you are sellign on j and buying on i
+                    {
+                        greatestProfit = prices[j] - prices[i];
+                        buyingPrice = prices[i];
+                        sellingPrice = prices[j];
+                    }
+                }
+                
+                return greatestProfit;
+            }
+            */
+        }
+    }
+}
